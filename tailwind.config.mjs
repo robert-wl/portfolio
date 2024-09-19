ae2768c6d0f098be4e27337f1a6d06d8b0abb6d0
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-09-19 08:15:11 +0700

    feat: added prettier and react

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontSize: {
+				'primary': '20px'
+			},
+			backgroundColor: {
+				'primary': '#2d323d',
+			},
+			borderColor: {
+				'primary': '#00ADB4',
+			},
+			boxShadow: {
+				'primary-shiny': '0 0 32px rgba(0, 173, 180, 0.5)',
+			},
+		},
+	},
+	plugins: [],
+}
