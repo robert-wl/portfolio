22c9066109349f8e0bbf81e8e66b29c85a429d51
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-09-22 19:43:37 +0700

    feat: work page temp commit

--- src/components/react/ProjectList.tsx
+++ src/components/react/ProjectList.tsx
@@ -1,12 +1,21 @@
-// interface IProps {
-//     personalProjects: IProject[];
-//     professionalProjects: IProject[];
-// }
+import { useState, type ReactNode } from "react";
 
-export default function ProjectList() {
+interface IProps {
+  personalProjects?: ReactNode;
+  professionalProjects?: ReactNode;
+}
+
+export default function ProjectList({ personalProjects, professionalProjects }: IProps) {
+  const [isProfessional, setIsProfessional] = useState(true);
+
   return (
-    <div>
-      <h1>Project List</h1>
+    <div className="flex flex-col gap-6">
+      <h1 className="pt-64 text-white text-4xl font-medium">Works and Projects I've Done</h1>
+      <div className="flex gap-16 text-white text-xl">
+        <button onClick={() => setIsProfessional(true)}>Professional</button>
+        <button onClick={() => setIsProfessional(true)}>Personal</button>
+      </div>
+      <div>{isProfessional ? professionalProjects : personalProjects}</div>
     </div>
   );
 }
--- src/models/project.d.ts
+++ src/models/project.d.ts
@@ -1,13 +1,9 @@
-
-
-
-
 export interface IProject {
-    title: string;
-    date: Date;
-    description: string;
-    techStack: string[];
-    githubUrl?: string;
-    projectUrl?: string;
-    imageUrl: string;
+  title: string;
+  date: Date;
+  description: string;
+  techStack: string[];
+  githubUrl?: string;
+  projectUrl?: string;
+  imageUrl: string;
 }
