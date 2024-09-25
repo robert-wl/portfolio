fa1501eae073887feaf49b449e216a59173393c8
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-09-25 13:22:44 +0700

    feat: added nodejs for docker deployment fix: fixed dockerfile for deployment

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,13 @@
 FROM node:lts AS runtime
-
 WORKDIR /app
 
 COPY . .
 
-RUN npm install -g pnpm
-
-RUN pnpm install
-RUN pnpm build
+RUN npm install
+RUN npm run build
 
 
+ENV HOST=0.0.0.0
+ENV PORT=4321
 EXPOSE 4321
-
-CMD node ./dist/server/entry.mjs
+CMD ["node",  "./dist/server/entry.mjs"]
--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,14 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  output: "server",
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
