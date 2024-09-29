f61ef735c7824704218366c73cb476f077208046
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-09-29 14:19:43 +0700

    feat: changed the density of the stars background

--- src/components/react/aceternity/stars-background.tsx
+++ src/components/react/aceternity/stars-background.tsx
@@ -20,7 +20,7 @@ interface StarBackgroundProps {
 }
 
 export const StarsBackground: React.FC<StarBackgroundProps> = ({
-  starDensity = 0.00015,
+  starDensity = 0.00085,
   allStarsTwinkle = true,
   twinkleProbability = 0.7,
   minTwinkleSpeed = 0.5,
