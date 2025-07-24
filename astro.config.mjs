bc2a8c7b3196f5bf872978e39f5508e530aaa5bc
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2025-07-24 09:22:34 +0700

    feat: added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  site: "https://robertwl.my.id",
+  integrations: [react(), tailwind(), sitemap()],
   output: "server",
 
   adapter: node({
