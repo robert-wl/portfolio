081475e4bd1ed4d34b8ec2429ef674b87f863b05
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2025-07-25 13:18:19 +0700

    style: linted everything

--- src/components/common/react/meteors.tsx
+++ src/components/common/react/meteors.tsx
@@ -17,7 +17,8 @@ export const Meteors = ({ number, className }: { number?: number; className?: st
             left: Math.floor(Math.random() * (400 - -400) + -400) + "px",
             animationDelay: Math.random() * (0.8 - 0.2) + 0.2 + "s",
             animationDuration: Math.floor(Math.random() * (10 - 2) + 2) + "s",
-          }}></span>
+          }}
+        />
       ))}
     </div>
   );
